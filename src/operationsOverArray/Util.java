c031e7b8c1b1591b417f949c7e53c2c430069e2e
AlbertoMGF <awefgaw@gmail.com>
2017-05-26 10:55:55 -0300

    Atualização no método getMax
    - Correção de bugs.
    - Implementação do padrão Comparator.
    - Arquivo de teste adicionado.

--- src/operationsOverArray/Util.java
+++ src/operationsOverArray/Util.java
@@ -1,21 +1,55 @@
 package operationsOverArray;
 
+import java.util.Comparator;
+
 public class Util <T extends Comparable<T>>  {
 	
 	public T getMax(T[] arr){
-		if (arr.length < 1) {
-			return null;
-		}else{
-			T max = arr[0];
+		return getMaxItem(arr, new MaxComparator<T>());
+	}
+	
+	private T getMaxItem(T[] arr, Comparator<T> comp){
+		if (isArrayValid(arr)) {
 			
+			T ObjectiveIten = arr[0];
 			for (int i = 0; i < arr.length; i++) {
-				if (max.compareTo(arr[i]) < 0) {
-					max = arr[i];
+				if (arr[i] != null) {
+					if (ObjectiveIten != null) {
+						if (comp.compare(ObjectiveIten, arr[i]) < 0) {
+							ObjectiveIten = arr[i];
+						}
+					}else{
+						ObjectiveIten = arr[i];
+					}
 				}
 			}
 			
-			return max;
-		}		
+			return ObjectiveIten;
+		}else{
+			return null;
+		}	
+	}
+	
+	private boolean isArrayValid(T[] arr){
+		if (arr != null) {
+			if (arr.length >= 1) {
+				if (getOccupedSize(arr) >= 1) {
+					return true;
+				}
+			}
+		}
+		return false;
+		
+	}
+	
+	private int getOccupedSize(T[] arr){
+		int Occupedsize = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != null) {
+				++Occupedsize;
+			}
+		}
+		return Occupedsize;
 	}
 
 }
---
+++ src/operationsOverArray/UtilTest.java
@@ -0,0 +1,70 @@
+package operationsOverArray;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class UtilTest {
+	public Integer[] array1;
+	public Integer[] array2;
+	public Integer[] array3;
+	public Integer[] array4;
+	
+	@Before
+	public void init(){
+		array2 = new Integer[1];
+		array3 = new Integer[10];
+		array4 = new Integer[0];
+	}
+
+	@Test
+	public void testA() {
+		Util<Integer> tool = new Util<Integer>();
+		Integer result = tool.getMax(array1);
+		Assert.assertEquals(result, null);
+	}
+	
+	@Test
+	public void testB() {
+		Util<Integer> tool = new Util<Integer>();
+		Integer result = tool.getMax(array2);
+		Assert.assertEquals(result, null);
+	}
+	
+	@Test
+	public void testC() {
+		array3[0] = new Integer(10);
+		Util<Integer> tool = new Util<Integer>();
+		Integer result = tool.getMax(array3);
+		Assert.assertEquals(result, new Integer(10));
+	}
+	
+	@Test
+	public void testD() {
+		array3[5] = new Integer(10);
+		Util<Integer> tool = new Util<Integer>();
+		Integer result = tool.getMax(array3);
+		Assert.assertEquals(result, new Integer(10));
+	}
+	
+	@Test
+	public void testE() {
+		Util<Integer> tool = new Util<Integer>();
+		Integer result = tool.getMax(array4);
+		Assert.assertEquals(result, null);
+	}
+	
+	@Test
+	public void testF() {
+		array3[2] = new Integer(10);
+		array3[9] = new Integer(-100);
+		array3[5] = new Integer(190);
+		array3[7] = new Integer(90);
+		Util<Integer> tool = new Util<Integer>();
+		Integer result = tool.getMax(array3);
+		Assert.assertEquals(result, new Integer(190));
+	}
+
+}
