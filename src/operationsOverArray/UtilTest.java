9f8b12efa78300093b18c4e4b6bc62b0ff4f0bd2
AlbertoMGF <awefgaw@gmail.com>
2017-05-26 11:39:48 -0300

    Função GetMin adicionada.
    Agora você pode encontrar o maior elemento em um Array.
    Os testes foram atualizados para cobrir essa funcionalidade.

--- src/operationsOverArray/UtilTest.java
+++ src/operationsOverArray/UtilTest.java
@@ -35,6 +35,13 @@ public class UtilTest {
 	
 	@Test
 	public void testC() {
+		Util<Integer> tool = new Util<Integer>();
+		Integer result = tool.getMax(array4);
+		Assert.assertEquals(result, null);
+	}
+	
+	@Test
+	public void testD() {
 		array3[0] = new Integer(10);
 		Util<Integer> tool = new Util<Integer>();
 		Integer result = tool.getMax(array3);
@@ -42,18 +49,11 @@ public class UtilTest {
 	}
 	
 	@Test
-	public void testD() {
+	public void testE() {
 		array3[5] = new Integer(10);
 		Util<Integer> tool = new Util<Integer>();
 		Integer result = tool.getMax(array3);
 		Assert.assertEquals(result, new Integer(10));
-	}
-	
-	@Test
-	public void testE() {
-		Util<Integer> tool = new Util<Integer>();
-		Integer result = tool.getMax(array4);
-		Assert.assertEquals(result, null);
 	}
 	
 	@Test
@@ -65,6 +65,33 @@ public class UtilTest {
 		Util<Integer> tool = new Util<Integer>();
 		Integer result = tool.getMax(array3);
 		Assert.assertEquals(result, new Integer(190));
+	}
+	
+	@Test
+	public void testG() {
+		array3[0] = new Integer(10);
+		Util<Integer> tool = new Util<Integer>();
+		Integer result = tool.getMin(array3);
+		Assert.assertEquals(result, new Integer(10));
+	}
+	
+	@Test
+	public void testH() {
+		array3[5] = new Integer(10);
+		Util<Integer> tool = new Util<Integer>();
+		Integer result = tool.getMin(array3);
+		Assert.assertEquals(result, new Integer(10));
+	}
+	
+	@Test
+	public void testI() {
+		array3[2] = new Integer(10);
+		array3[9] = new Integer(-100);
+		array3[5] = new Integer(190);
+		array3[7] = new Integer(90);
+		Util<Integer> tool = new Util<Integer>();
+		Integer result = tool.getMin(array3);
+		Assert.assertEquals(result, new Integer(-100));
 	}
 
 }
